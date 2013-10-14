67942fda53a89c01e5c1dae40ea31766b73039bf
Ben Guo <ben@venmo.com>
2013-10-14 00:32:05 -0400

    Revises docstrings

--- BZGFormField/BZGFormField.h
+++ BZGFormField/BZGFormField.h
@@ -28,56 +28,44 @@ typedef BOOL (^BZGTextValidationBlock)(NSString *text);
 @property (strong, nonatomic) UIButton *leftIndicator;
 @property (strong, nonatomic) UIAlertView *alertView;
 
-/**
- The width of the left indicator when inactive, relative to the height of the form field.
- */
+/// The width of the left indicator when inactive, relative to the height of the form field.
 @property (assign, nonatomic) CGFloat leftIndicatorInactiveWidth;
 
-/**
- The width of the left indicator when active, relative to the height of the form field.
- */
+/// The width of the left indicator when active, relative to the height of the form field.
 @property (assign, nonatomic) CGFloat leftIndicatorActiveWidth;
 
-/**
- The padding between the left indicator and the text field, relative to the height of the form field.
- */
+/// The padding between the left indicator and the text field, relative to the height of the form field.
 @property (assign, nonatomic) CGFloat leftIndicatorRightPadding;
 
-/**
- The color of the left indicator when the form is invalid.
- */
+/// The color of the left indicator when the form is invalid.
 @property (strong, nonatomic) UIColor *leftIndicatorInvalidColor;
 
-/**
- The color of the left indicator when the form is valid.
- */
+/// The color of the left indicator when the form is valid.
 @property (strong, nonatomic) UIColor *leftIndicatorValidColor;
 
-/**
- The color of the left indicator when the form is neither invalid nor valid.
- */
+/// The color of the left indicator when the form is neither invalid nor valid.
 @property (strong, nonatomic) UIColor *leftIndicatorNoneColor;
 
 /**
- Returns the current state of the form (invalid | valid | none)
+ * Returns the form's current validation state (invalid | valid | none)
  */
 - (BZGFormFieldState)formFieldState;
 
 /**
- Returns the current state of the left indicator (inactive | active)
+ * Returns the current state of the field's left indicator (inactive | active)
  */
 - (BZGLeftIndicatorState)leftIndicatorState;
 
 /**
- Sets the validation block for the text field.
-  */
+ * Sets the validation block for the text field.
+ */
 - (void)setTextValidationBlock:(BZGTextValidationBlock)block;
 
 @end
 
 /**
- At the moment, a form field just forwards all UITextFieldDelegate and UIAlertViewDelegate messages to its delegate.
-*/
+ * At the moment, a form field just forwards all UITextFieldDelegate and UIAlertViewDelegate messages to its delegate.
+ */
 @protocol BZGFormFieldDelegate <UITextFieldDelegate, UIAlertViewDelegate>
 
 @end
